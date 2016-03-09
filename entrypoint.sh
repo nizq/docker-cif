67ce111372474812afcecdcb6d8fbeb6d05c94ac
尼志强 <ni.zhiqiang@gmail.com>
2016-03-09 14:58:34 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 es_host=${ES:-localhost}
-new_mark=/etc/cif/.new_install
+new_mark=/var/cif/.new_install
 
 url="http://${es_host}:9200/_template/cif_observables/"
 num=$(curl -XGET ${url} 2>/dev/null|jq '.cif_observables|length')
@@ -19,7 +19,7 @@ if [ "$num" -eq 0 ]; then
 fi
 
 for service in "worker" "smrt"; do
-    conf=/etc/cif/cif-${service}.yml
+    conf=/var/cif/cif-${service}.yml
     if [ ! -f "$conf" ] || [ -f "new_mark" ]; then
         /opt/cif/bin/cif-tokens --storage-host ${es_host}:9200 \
                                 --username cif-${service} --new --read --write \
@@ -37,10 +37,10 @@ cat <<EOF >/supervisord.conf
 command=/opt/cif/bin/cif-router --storage-host=${es_host}:9200 --logging --logfile=/var/log/cif/router.log
 
 [program:worker]
-command=/opt/cif/bin/cif-worker -C /etc/cif/bin/cif-worker.yml --logging --logfile=/var/log/cif/worker.log
+command=/opt/cif/bin/cif-worker -C /var/cif/cif-worker.yml --logging --logfile=/var/log/cif/worker.log
 
 [program:smrt]
-command=/opt/cif/bin/cif-smrt -C /etc/cif/bin/cif-smrt.yml --logging --logfile=/var/log/cif/smrt.log
+command=/opt/cif/bin/cif-smrt -C /var/cif/cif-smrt.yml --logging --logfile=/var/log/cif/smrt.log
 
 [supervisord]
 logfile = /var/log/cif/supervisord.log
